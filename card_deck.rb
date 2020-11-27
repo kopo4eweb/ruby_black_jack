5bcc2cae7759d81f79e97717f00757668304e265
Artem Nikolenko <kopo4e.web@gmail.com>
2020-11-27 22:35:10 +0300

    Upd: refactor class CardDeck

--- card_deck.rb
+++ card_deck.rb
@@ -4,6 +4,23 @@
 class CardDeck
   SYMBOLS = ['♡', '♤', '♢', '♧'].freeze
   VALS = %w[2 3 4 5 6 7 8 9 10 V D K T].freeze
+  VAL_CARD_T = 11
+  VAL_CARD_PUCTURE = 10
+  MAX_SUM = 21
+
+  def self.calculate(cards)
+    sum = 0
+    t = 0
+
+    cards.each do |card|
+      sum += card[:val]
+      t += 1 if card[:name] == 'T'
+    end
+
+    sum -= VAL_CARD_T if sum > MAX_SUM && t.positive?
+
+    sum
+  end
 
   def initialize
     @pack = []
@@ -24,9 +41,9 @@ class CardDeck
   def genegate
     SYMBOLS.each do |symbol|
       VALS.each do |val|
-        var_val = val.to_i.positive? ? val.to_i : 10
-        var_val = 11 if val == 'T'
-        @pack << { "#{val}#{symbol}" => { name: val, val: var_val } }
+        var_val = val.to_i.positive? ? val.to_i : VAL_CARD_PUCTURE
+        var_val = VAL_CARD_T if val == 'T'
+        @pack << { symbol: "#{val}#{symbol}", name: val, val: var_val }
       end
     end
   end
