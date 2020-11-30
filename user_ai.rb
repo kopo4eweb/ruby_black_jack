eadf0a67600d4f20202a5b6735b31da2944b9414
Artem Nikolenko <kopo4e.web@gmail.com>
2020-11-30 21:14:51 +0300

    Upd: refactor Ai

--- user_ai.rb
+++ user_ai.rb
@@ -2,15 +2,15 @@
 
 # Ai-user for dealer
 class UserAi
-  MAX_SUM = 17
+  DEALER_MAX_SUM = 17
 
   class << self
     def thinking(user)
       sum = CardDeck.sum_card(user.cards)
 
-      if sum >= MAX_SUM
-        user.skip_turn
-      else
+      if sum >= DEALER_MAX_SUM
+        user.skip_turn if user.may_skip?
+      elsif user.may_get_card?
         user.add_card(GameInterface.new_card)
       end
     end
